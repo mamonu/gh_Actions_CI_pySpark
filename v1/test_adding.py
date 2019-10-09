b49ff7bd94c0be9211421b2fbd759d51fb700dfa
mamonu <thorosm@msn.com>
2019-10-09 14:01:29 +0100

    example spark basic code

--- v1/test_adding.py
+++ v1/test_adding.py
@@ -1,10 +1,34 @@
 import helloworld
 import pytest
+from pyspark import SparkContext, SparkConf
+from pyspark.sql import SparkSession
+from pyspark.sql import types
 
-print(dir(helloworld))
+
+# Create some example data
+data = [
+    ('Batmobile', 7, 5.0),
+    ('Batmobile', 2, 0.0),
+    ('Batmobile', 3, 5.0),
+    ('Catmobile', 7, 5.0),
+    ('Catmobile', 2, 5.0),
+    ('Catmobile', 3, 9.0),
+  
+]
+
+custom_schema = types.StructType()
+custom_schema.add(types.StructField("my_string", types.StringType()))
+custom_schema.add(types.StructField("my_integer", types.IntegerType()))
+custom_schema.add(types.StructField("my_float", types.FloatType()))
+
+
 
 
 def test_add():
     assert (helloworld.add(1,1)==2)
+
+
+
+
     
 
