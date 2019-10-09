dae1a72ad4e67acda26f1e032c379b86bf74a835
mamonu <thorosm@msn.com>
2019-10-09 14:35:27 +0100

    added to the example spark basic code

--- v1/helloworld.py
+++ v1/helloworld.py
@@ -1,3 +1,29 @@
+
+
+
+def vehicle_clean(df, spark):
+    """
+    """
+
+    df.registerTempTable('df')
+    sql = """
+    SELECT *,
+     CASE WHEN Vehicle IS NULL THEN 'Not known'
+         ELSE Vehicle
+     END as der_name
+    from df
+    """
+
+    df = spark.sql(sql)
+    return df   
+
+
+
+
+
 def add(a,b):
     c = a + b
     return c
+
+
+
--- v1/test_adding.py
+++ v1/test_adding.py
@@ -3,26 +3,25 @@ import pytest
 from pyspark import SparkContext, SparkConf
 from pyspark.sql import SparkSession
 from pyspark.sql import types
+sc = SparkContext.getOrCreate()
+spark = SparkSession(sc)
+
 
 
 # Create some example data
 data = [
     ('Batmobile', 7, 5.0),
-    ('Batmobile', 2, 0.0),
-    ('Batmobile', 3, 5.0),
-    ('Catmobile', 7, 5.0),
-    ('Catmobile', 2, 5.0),
     ('Catmobile', 3, 9.0),
-  
+    ('', 4, 21.0),
 ]
 
 custom_schema = types.StructType()
-custom_schema.add(types.StructField("my_string", types.StringType()))
-custom_schema.add(types.StructField("my_integer", types.IntegerType()))
-custom_schema.add(types.StructField("my_float", types.FloatType()))
+custom_schema.add(types.StructField("Vehicle", types.StringType()))
+custom_schema.add(types.StructField("wheels", types.IntegerType()))
+custom_schema.add(types.StructField("speed", types.FloatType()))
 
 
-
+df = spark.createDataFrame(data, custom_schema)
 
 def test_add():
     assert (helloworld.add(1,1)==2)
@@ -30,5 +29,14 @@ def test_add():
 
 
 
+
+def test_data_count_check():
+    assert (df.count()==3)
+
+
+
+
+
+    
     
 
